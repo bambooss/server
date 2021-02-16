a1710cbb755224ea57cad69b8c25ff52b1396ccb
Gabor Csecsetka <csecsi85@gmail.com>
2021-02-16 23:16:47 +0000

    Changed get token from code to be handled by the front-end

--- src/routes/router-index.ts
+++ src/routes/router-index.ts
@@ -4,25 +4,25 @@ const router = express.Router()
 import axios from 'axios'
 
 router.get('/github/callback', async (req: Request, res: Response) => {
-  console.log('code: ', req.query.code)
-  // The req.query object has the query params that were sent to this route.
-  const requestToken = req.query.code
+  // console.log('code: ', req.query.code)
+  // // The req.query object has the query params that were sent to this route.
+  // const requestToken = req.query.code
+  //
+  // const response: any = await axios({
+  //   method: 'post',
+  //   url: `https://github.com/login/oauth/access_token?client_id=${process.env.GH_CLIENT_ID}&client_secret=${process.env.GH_CLIENT_SECRET}&code=${requestToken}`,
+  //   headers: {
+  //     accept: 'application/json'
+  //   }
+  // })
 
-  const response: any = await axios({
-    method: 'post',
-    url: `https://github.com/login/oauth/access_token?client_id=${process.env.GH_CLIENT_ID}&client_secret=${process.env.GH_CLIENT_SECRET}&code=${requestToken}`,
-    headers: {
-      accept: 'application/json'
-    }
-  })
-
-  console.log('token: ', response.data.access_token)
+  console.log('token: ', req.headers.authorization)
 
   const user= await axios( {
     method: 'get',
     url: 'https://api.github.com/user',
     headers: {
-      Authorization: 'token ' + response.data.access_token
+      Authorization: 'token ' + req.headers.authorization
     }
   })
 
