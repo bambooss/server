31f41fb619499a12defd5fc11db0b8adc0531994
Gabor Csecsetka <csecsi85@gmail.com>
2021-02-21 10:37:50 +0000

    Added profile creation from usernames

--- src/api/controllers/controller-user.ts
+++ src/api/controllers/controller-user.ts
@@ -14,6 +14,7 @@ exports.createUser = async (req: express.Request, res: express.Response) => {
       })
     }
 
+    // Compare passwords
     if(user.password !== user.password_confirmation) {
       return res.status(400).json({
         status: 400,
@@ -21,9 +22,15 @@ exports.createUser = async (req: express.Request, res: express.Response) => {
       })
     }
 
+    // Hash password
     user.password = await bcrypt.hash(user.password, 11)
 
+    // Verify and create social profiles
+    user = verifyAndCreateSocial(user)
+
+    // Create user model
     user = await model_users.create(user)
+    // Generate auth token
     const token = await user.generateAuthToken()
 
     return res.status(200).json({
@@ -39,4 +46,20 @@ exports.createUser = async (req: express.Request, res: express.Response) => {
       message: 'Server error 500'
     })
   }
+}
+
+const verifyAndCreateSocial = (user: { profiles: { githubURL: string; gitlabURL: string; bitbucketURL: string; linkedinURL: string } }) => {
+  if(user.profiles.githubURL !== '') {
+    user.profiles.githubURL = `https://github.com/${user.profiles.githubURL}`
+  }
+  if(user.profiles.gitlabURL !== '') {
+    user.profiles.gitlabURL = `https://gitlab.com/${user.profiles.gitlabURL}`
+  }
+  if(user.profiles.bitbucketURL !== '') {
+    user.profiles.bitbucketURL = `https://bitbucket.org/${user.profiles.bitbucketURL}/`
+  }
+  if(user.profiles.linkedinURL !== '') {
+    user.profiles.linkedinURL = `https://www.linkedin.com/in/${user.profiles.linkedinURL}/`
+  }
+  return user
 }
