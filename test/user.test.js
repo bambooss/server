265edab727d3fab47665fbde18c42c880a658b30
Gabor Csecsetka <csecsi85@gmail.com>
2021-03-02 03:55:37 +0000

    Delete user tests complete

--- test/user.test.js
+++ test/user.test.js
@@ -351,4 +351,73 @@ describe('Users', () => {
         })
     })
   })
+
+  describe('DELETE /user', () => {
+    let createdUser = {}
+    const getUserProfile = async () => {
+      await model_user.findById(createdUser._id)
+    }
+
+    it('it should create a new user (All fields correct)', (done) => {
+      const body = {
+        user: {
+          username: 'Csecsi',
+          email: 'csecsi55@gmail.com',
+          password: '12345678a',
+          confirmPassword: '12345678a',
+          githubURL: 'Csecsi85',
+          gitlabURL: 'csecsi85',
+          bitbucketURL: 'csecsi',
+          linkedinURL: 'gabor-csecsetka-539765111',
+          technologies: ['HTML', 'CSS', 'JS'],
+          languages: ['Hungarian', 'English', 'Spanish'],
+          bio: 'This is my test bio'
+        }
+      }
+
+      chai.request(server)
+        .post('/user/register')
+        .send(body)
+        .end((err, res) => {
+          expect(res, 'res.status should be 201').to.have.status(201)
+          expect(res.body.user, 'req.body.user is missing keys').to.be.an('object').to.include.all.keys('_id', 'username', 'email', 'password', 'isDeleted', 'avatar', 'githubURL', 'gitlabURL', 'bitbucketURL', 'linkedinURL', 'technologies', 'languages', 'bio', 'tokens', 'createdAt', 'updatedAt', '__v', 'resetPasswordToken')
+          expect(res.body.user.username, 'Usernames don\'t match').to.equal(body.user.username)
+          expect(res.body.user.email, 'Emails don\'t match').to.equal(body.user.email)
+          expect(res.body.user.password, 'Password should be hidden').to.equal('***********')
+          expect(res.body.user.githubURL, 'GitHub URL is not correct').to.equal(`https://github.com/${body.user.githubURL}`)
+          expect(res.body.user.gitlabURL, 'GitLab URL is not correct').to.equal(`https://gitlab.com/${body.user.gitlabURL}`)
+          expect(res.body.user.bitbucketURL, 'BitBucket URL is not correct').to.equal(`https://bitbucket.org/${body.user.bitbucketURL}/`)
+          expect(res.body.user.linkedinURL, 'LinkedIn URL is not correct').to.equal(`https://www.linkedin.com/in/${body.user.linkedinURL}/`)
+          expect(res.body.user.technologies, 'Technologies don\'t match').to.be.deep.equal(body.user.technologies)
+          expect(res.body.user.languages, 'Languages don\'t match').to.be.deep.equal(body.user.languages)
+          expect(res.body.user.bio, 'Bios don\'t match').to.be.deep.equal(body.user.bio)
+          expect(res.body.user.isDeleted, 'isDeleted should be false').to.be.false
+          expect(res.body.user.tokens, 'Token is missing').to.have.length(1)
+          createdUser = res.body.user
+          done()
+        })
+    })
+
+    it('it should respond with a status 200', (done) => {
+      chai.request(server)
+        .delete('/user')
+        .set('Authorization', `Bearer ${createdUser.tokens[0].token}`)
+        .end((err, res) => {
+          expect(res, 'res.status should be 200').to.have.status(200)
+          expect(res.body, 'res.body is missing status or message field').to.have.keys('status', 'message')
+          expect(res.body, 'res.body has user or token fields').not.to.have.keys('user', 'token')
+          done()
+        })
+    })
+
+    it('it should have isDeleted: true', (done) => {
+      chai.request(server)
+        .get('/user')
+        .set('Authorization', `Bearer ${createdUser.tokens[0].token}`)
+        .end((err, res) => {
+          expect(res.body.user.isDeleted, 'isDeleted should be true').to.be.true
+          done()
+        })
+    })
+  })
 })
