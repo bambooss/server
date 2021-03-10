73c409232ee7a2ff48aaecf17bd20bfbfed6e016
Gabor Csecsetka <csecsi85@gmail.com>
2021-03-10 00:09:52 +0000

    Added delete project by ID controller and route

--- src/api/controllers/controller-job.ts
+++ src/api/controllers/controller-job.ts
@@ -8,6 +8,7 @@ const model_job = require('../models/model-job')
 
 exports.createJob = async (req: Request, res: Response) => {
   try {
+    // Prepare job details
     const jobDetails = {
       title: req.body.job.title,
       sortTitle: req.body.job.title.toLowerCase(),
@@ -16,11 +17,13 @@ exports.createJob = async (req: Request, res: Response) => {
       project: req.body.job.projectId
     }
 
+    // Check if user is the actual owner of the project
     const verifiedProject = await model_projects.findOne({
       owner: req.body.decoded._id,
       _id: req.body.job.projectId
     })
 
+    // If not verified
     if (!verifiedProject) {
       return res.status(400).json({
         status: 400,
@@ -28,8 +31,10 @@ exports.createJob = async (req: Request, res: Response) => {
       })
     }
 
+    // Creat job
     const createdJob = await model_job.create(jobDetails)
 
+    //If there was a problem
     if (!createdJob) {
       return res.status(400).json({
         status: 400,
@@ -37,6 +42,7 @@ exports.createJob = async (req: Request, res: Response) => {
       })
     }
 
+    // Add job ID to corresponding technology
     await model_technology.updateMany(
       { name: createdJob.technologies },
       { $push: { jobs: createdJob._id } }
@@ -58,8 +64,11 @@ exports.createJob = async (req: Request, res: Response) => {
 
 exports.getJobById = async (req: Request, res: Response) => {
   try {
+    // Get job Id
     const jobId = req.params.id
+    // Find job by ID and populate the project
     const job = await model_job.findById(jobId).populate('project')
+    // If there was no project was found with this ID
     if (!job) {
       return res.status(404).json({
         status: 404,
@@ -140,6 +149,52 @@ exports.getAllJobs = async (req: Request, res: Response) => {
         message: 'Invalid type of input'
       })
     }
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      status: 500,
+      message: error.message
+    })
+  }
+}
+
+exports.deleteJobById = async (req: Request, res: Response) => {
+  try {
+    // Get job ID from the query string
+    const jobId = req.params.id
+    // Get the user ID from the token
+    const userId = req.body.decoded._id
+
+    // Find job by id and populate project
+    const job = await model_job.findOne({_id: jobId}).populate('project')
+
+    // If a job found
+    if(job) {
+      // Verify if the user is the project owner
+      if(job.project.owner == userId) {
+        // Delete job from DB
+        const deletedJob = await model_job.findOneAndRemove({_id: jobId})
+        // If job deleted
+        if (deletedJob) {
+          // Remove job ID from technologies
+          await model_technology.updateMany(
+              { jobs: jobId },
+              { $pull: { jobs: jobId } }
+            )
+
+            return res.status(200).json({
+              status: 200,
+              message: 'Job successfully deleted'
+            })
+        }
+      }
+    }
+
+    // If no job found
+    return res.status(400).json({
+      status: 400,
+      message: 'Job not found'
+    })
   } catch (error) {
     console.log(error)
     return res.status(500).json({
--- src/routes/router-job.ts
+++ src/routes/router-job.ts
@@ -4,13 +4,15 @@ const {
   createJob,
   getJobById,
   getAllJobs,
+  deleteJobById,
 } = require('../api/controllers/controller-job')
 const { auth } = require('../middlewares/middleware-auth')
 
 const router = express.Router()
 
 router.post('/', auth, createJob)
-router.get('/:id', auth, getJobById)
 router.get('/', auth, getAllJobs)
+router.get('/:id', auth, getJobById)
+router.delete('/:id', auth, deleteJobById)
 
 module.exports = router
